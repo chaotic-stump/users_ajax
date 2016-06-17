040680dd4d0844ecae3251e17233781ab66c581c
Shaws <seanrichardparker@gmail.com>
2016-06-17 16:00:58 -0600

    adding show view and getting table to work

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,6 +1,4 @@
 
-
-
 $(document).ready(function(){
   var baseUrl =
   'http://devpoint-ajax-example-server.herokuapp.com/api/v1/users';
@@ -9,17 +7,85 @@ $(document).ready(function(){
     function getUsers() {
       $.ajax({
         url: baseUrl,
-        type: 'GET'
+        type: 'GET',
         dataType: 'JSON'
       }).done(function(data) {
         var tbody = $('#users');
         tbody.children().remove();
-        data.users.forEach(
-        function(user) {
-          var 
-        }
-        )
+        data.users.forEach(function(user) {
+          var firstName = user.first_name ? user.first_name : '';
+          var lastName = user.last_name ? user.last_name : '';
+          var phoneNumber = user.phone_number ? user.phone_number : '';
+          var row = '<tr data-id="' + user.id + '"><td>' + user.first_name + '</td>';
+          row += '<td>' + lastName + '</td>';
+          row += '<td>' + phoneNumber + '</td>';
+          row += '<td>'
+          row += '<button class="btn btn-danger delete">Delete</button>';
+          row += '<button class="btn btn-primary show">Show</button>';
+          row += '</td>';
+          row += '</tr>';
+          tbody.append(row);
+        });
+      }).fail( function(err) {
+        alert('Something went wrong call support');
+      });
+    }
+
+    getUsers();
+
+    $(document).on('click', '.delete', function() {
+      var id = $(this).closest('tr').data().id;
+      deleteUser(id);
+    });
+
+    $(document).on('click', '.show', function() {
+      var id = $(this).closest('tr').data().id;
+      location.pathname = '/welcome/' + id;
+    })
+
+    function deleteUser(id) {
+      $.ajax({
+        url: baseUrl + '/' + id,
+        type: 'DELETE'
+      }).done( function() {
+        getUsers();
+      }).fail( function(err) {
       })
     }
+
+  } //root route
+
+  var re = /\/welcome\/\d+/;
+  if (location.pathname.match(re)) {
+    var panel = $('#panel');
+    var id = panel.data().id;
+    $.ajax({
+      url: baseUrl + '/' + id,
+      type: 'GET',
+      dataType: 'JSON'
+    }).done( function(data) {
+      var user = data.user;
+      panel.children('#heading').html(user.id);
+      var list = $('#user');
+      var fname = '<li>First Name: ' + user.first_name + '</li>';
+      var lname = '<li>Last Name: ' + user.last_name + '</li>';
+      var pnum = '<li>Phone Number: ' + user.phone_number + '</li>';
+      list.append(fname);
+      list.append(lname);
+      list.append(pnum)
+    })
   }
+
+  $('#new_user').on('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: baseUrl,
+      type: 'POST',
+      dataType: 'JSON',
+      data: $(this).serializeArray()
+    }).done( function() {
+      location.pathname = '/';
+    });
+  })
+
 })
